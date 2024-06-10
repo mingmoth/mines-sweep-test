bbe38a8524df9c4f83d24a5a845186f04d6698b7
mingmoth <mingmoth@gmail.com>
2024-06-10 11:16:06 +0800

    feat: add useTimer composable

---
+++ src/useTimer.js
@@ -0,0 +1,38 @@
+import { computed, ref, toRefs, watch } from 'vue'
+
+export default function useTimer() {
+
+    const start = ref(null)
+    const end = ref(null)
+    const interval = ref(null)
+
+    const seconds = computed(() => {
+        return Math.round((end.value - start.value) / 1000)
+    })
+
+    function startCount() {
+        start.value = +Date.now()
+        end.value = +Date.now()
+    }
+
+    function endCount() {
+        clearInterval(interval.value)
+    }
+
+    function count() {
+        if(interval.value) return
+
+        interval.value = setInterval(() => {
+            end.value = +Date.now()
+        }, 1000)
+    }
+
+    return {
+        start,
+        end,
+        seconds,
+        count,
+        endCount,
+        startCount
+    }
+}
